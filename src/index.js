f3856b96d8b00944c40c6a16ebe2659231476af0
Javier Valles <deliverst@icloud.com>
2022-02-16 18:59:31 -0500

    connection with api rest cours 5

--- src/index.js
+++ src/index.js
@@ -1,6 +1,14 @@
 const http = require('http')
+const express = require('express')
+const app = express()
+const logger = require('./loggerMiddleware')
+const cors = require('cors')
 
-const notes = [{
+
+app.use(cors())
+
+let notes = [
+    {
         userId: 1,
         id: 1,
         title: "sunt aut facere repellat provident occaecati excepturi optio reprehenderit",
@@ -20,13 +28,82 @@ const notes = [{
     }
 ]
 
-const app = http.createServer((req, res) => {
-    res.writeHead(200, { 'Content-Type': 'application/json' })
-    // res.end('como estas muchacho')
-    res.end(JSON.stringify(notes))
+// MIDELWARES
+app.use(express.json())
+app.use(logger)
+
+
+// HOME
+app.get('/', (req, res) => {
+    res.send('<h1>Hola muchachin</h1>')
+})
+
+//GET ALL NOTES
+app.get('/api/notes', (req, res) => {
+    res.json(notes)
+})
+
+// GET
+app.get('/api/notes/:id', (req, res) => {
+    const {id} = req.params
+    const note = notes.find(notes => notes.id === Number(id))
+
+    if (note) {
+        res.json(note)
+    } else {
+        res.status(404).end()
+    }
 
 })
 
-const PORT = 3000
-app.listen(PORT)
-console.log(`Se esta escuchando por el puerto ${PORT}`)
+//DELETE
+app.delete('/api/delete/:id', (req, res) => {
+    const {id} = req.params
+    console.log(id)
+    notes = notes.filter(note => note.id !== Number(id))
+    res.status(204).end()
+})
+
+// POST
+app.post('/api/notes', (req, res) => {
+    let info = req.body
+
+    if (!info || !req.body.body || !req.body.title) {
+        return res.status(400).json({error: 'note.content is missing'})
+
+    }
+    const ids = notes.map(note => note.id)
+    const maxId = Math.max(...ids)
+    console.log(maxId)
+
+    const newNote = {
+        userId: 1,
+        id: maxId + 1,
+        title: req.body.title || '',
+        body: req.body.body,
+        date: new Date().toLocaleString()
+    }
+
+    console.log(newNote)
+    notes = [...notes, newNote]
+
+    res.status(201).json(newNote)
+})
+
+app.use((req, res) => {
+    res.status(404).json({
+        error: 'Not Found'
+    })
+    console.log(req.path)
+})
+
+const PORT = 3001
+app.listen(PORT, () => {
+    console.log(`Se esta escuchando por el puerto ${PORT}`)
+})
+
+// const app = http.createServer((req, res) => {
+//     res.writeHead(200, { 'Content-Type': 'application/json' })
+//     // res.end('como estas muchacho')
+//     res.end(JSON.stringify(notes))
+// })
